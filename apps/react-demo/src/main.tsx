8c8b35c9ddd54e5fc922a7fef5a63231f52fb6e9
Boris Belmar <borisbelmarm@gmail.com>
2024-09-26 21:57:05 -0300

    Add core to dependencies

--- apps/react-demo/src/main.tsx
+++ apps/react-demo/src/main.tsx
@@ -7,6 +7,6 @@ initArrowNavigation({
   debug: true
 })
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
 )
